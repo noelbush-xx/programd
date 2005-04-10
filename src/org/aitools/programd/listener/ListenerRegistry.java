a80b4d82424b8e8456bdb08a4fbb459ea1f84849
noel <>
2005-04-10 16:11:46 +0000

    changed to use parameterized ClassRegistry

--- src/org/aitools/programd/listener/ListenerRegistry.java
+++ src/org/aitools/programd/listener/ListenerRegistry.java
@@ -9,7 +9,6 @@
 
 package org.aitools.programd.listener;
 
-import org.aitools.programd.Core;
 import org.aitools.programd.util.ClassRegistry;
 
 /**
@@ -18,31 +17,18 @@ import org.aitools.programd.util.ClassRegistry;
  * @since 4.1.3
  * @author Noel Bush
  */
-public class ListenerRegistry extends ClassRegistry
+public class ListenerRegistry extends ClassRegistry<Listener>
 {
     /** The list of processors (fully-qualified class names). */
     private static final String[] PROCESSOR_LIST =
         { "org.aitools.programd.listener.AIMListener", "org.aitools.programd.listener.ICQListener",
                 "org.aitools.programd.listener.IRCListener" } ;
-
-    /** The fully-qualified name of {@link Listener} . */
-    private static final String PROCESSOR_BASE_CLASS_NAME = "org.aitools.programd.listener.Listener";
-
-    /** The private member that initializes this class. */
-    private static final ListenerRegistry self = new ListenerRegistry();
-
-    private ListenerRegistry()
-    {
-        super(Core.VERSION, PROCESSOR_LIST, PROCESSOR_BASE_CLASS_NAME);
-    } 
-
+    
     /**
-     * Returns the ListenerRegistry's singelton self.
-     * 
-     * @return ListenerRegistry
+     * Creates a new <code>ListenerRegistry</code>.
      */
-    public static ListenerRegistry getSelf()
+    public ListenerRegistry()
     {
-        return self;
+        super(PROCESSOR_LIST);
     } 
 }
