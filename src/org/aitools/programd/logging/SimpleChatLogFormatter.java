56f5c9b76422049956aa5336c9836b57751b2daf
noel <>
2005-04-10 16:16:39 +0000

    the complement to XMLChatLogFormatter

---
+++ src/org/aitools/programd/logging/SimpleChatLogFormatter.java
@@ -0,0 +1,62 @@
+/*
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version. You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+package org.aitools.programd.logging;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.LogRecord;
+import java.util.logging.SimpleFormatter;
+
+import org.aitools.programd.logging.ChatLogRecord;
+
+/**
+ * Formats a ChatLogRecord by printing a number of extra fields as we like them.
+ * @author Noel Bush
+ * @since 4.5
+ */
+public class SimpleChatLogFormatter extends SimpleFormatter
+{
+    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+
+    private static final String RBRACKET_SPACE = "] ";
+
+    private static final String RANGLE_BRACKET_SPACE = "> ";
+
+    private static final String LINE_SEPARATOR = System.getProperty("line.separator", "\n");
+
+    /**
+     * We insist that the record be a ChatLogRecord.
+     * @see java.util.logging.SimpleFormatter#format
+     * @param record the ChatLogRecord to format
+     * @return the result of formatting the given ChatLogRecord
+     * @throws IllegalArgumentException if the record is not a ChatLogRecord
+     */
+    public String format(LogRecord record)
+    {
+        if (!(record instanceof ChatLogRecord))
+        {
+            throw new IllegalArgumentException("XMLChatLogFormatter is intended to handle ChatRecords only.");
+        }
+        return format((ChatLogRecord) record);
+    }
+
+    /**
+     * @see java.util.logging.XMLFormatter#format
+     * @param record the ChatLogRecord to format
+     * @return the result of formatting the given ChatLogRecord
+     */
+    public String format(ChatLogRecord record)
+    {
+        String datetime = dateFormatter.format(new Date(record.getMillis()));
+        return '[' + datetime + RBRACKET_SPACE + record.getUserID() + RANGLE_BRACKET_SPACE + record.getInput() + LINE_SEPARATOR + '[' + datetime
+                + RBRACKET_SPACE + record.getBotID() + RANGLE_BRACKET_SPACE + record.getReply();
+    }
+
+}
