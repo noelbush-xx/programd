9ca7c7650f6125805c676987ad9292ac8e296882
noel <>
2005-04-10 23:19:17 +0000

    added use of registry of servlet request responders, and removed GNU license, since this depends on Jetty, which is under the incompatible Apache 2.0 license

--- src/org/aitools/programd/server/jettyinterface/ProgramDAwareJettyServer.java
+++ src/org/aitools/programd/server/jettyinterface/ProgramDAwareJettyServer.java
@@ -1,38 +1,47 @@
-/*
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version. You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
- */
-
 package org.aitools.programd.server.jettyinterface;
 
 import org.aitools.programd.Core;
+import org.aitools.programd.server.ServletRequestResponderManagerRegistry;
 
 import org.mortbay.http.HttpContext;
+import org.mortbay.jetty.servlet.ServletHttpContext;
 import org.mortbay.jetty.Server;
 
 /**
  * This subclass of the Jetty server includes a reference to a Core object as one
- * of its attributes, so that servlets can get access to the Core.
- * @author Noel Bush
+ * of its attributes, so that servlets can get access to the Core.  The Core object reference
+ * taken by the constructor is saved so that each time a context is added, the
+ * Core object is inserted as an attribute of the context.
+ * @author <a href="mailto:noel@aitools.org">Noel Bush</a>
  * @since 4.5
  */
 public class ProgramDAwareJettyServer extends Server
 {
+    private Core core;
+    
+    private ServletRequestResponderManagerRegistry responderRegistry;
+    
     /**
-     * Creates a new ProgramDAwareJettyServer and attaches the given Core.
-     * @param core the Core to attach.
+     * Creates a new ProgramDAwareJettyServer and keeps a reference the given Core
+     * and the given responder registry.
+     * @param coreToUse the Core to use.
+     * @param registry the responder registry to use
      */
-    public ProgramDAwareJettyServer(Core core)
+    public ProgramDAwareJettyServer(Core coreToUse, ServletRequestResponderManagerRegistry registry)
     {
         super();
-        HttpContext[] contexts = this.getContexts();
-        for (int index = 0; index < contexts.length; index++)
-        {
-            contexts[index].setAttribute("core", core);
-        }
+        this.core = coreToUse;
+        this.responderRegistry = registry;
+    }
+
+    /**
+     * @see org.mortbay.http.HttpServer#addContext(org.mortbay.http.HttpContext)
+     */
+    public HttpContext addContext(HttpContext context)
+    {
+        ServletHttpContext result = (ServletHttpContext)super.addContext(context);
+        result.getServletContext().setAttribute("core", this.core);
+        result.getServletContext().setAttribute("responder-registry", this.responderRegistry);
+        return result;
     }
 }
