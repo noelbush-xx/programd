540a37f0de7f3603a94b8db73b33ecfd255ec1f3
noel <>
2005-05-11 18:24:24 +0000

    made this an interface (again)

--- src/org/aitools/programd/listener/Listener.java
+++ src/org/aitools/programd/listener/Listener.java
@@ -9,54 +9,17 @@
 
 package org.aitools.programd.listener;
 
-import java.util.Map;
-import java.util.logging.Logger;
-
-import org.aitools.programd.Core;
-import org.aitools.programd.bot.Bot;
 import org.aitools.programd.util.ManagedProcess;
 
 /**
  * An abstract listener.
  */
-public abstract class Listener implements ManagedProcess
+public interface Listener extends ManagedProcess
 {
-    /** The Core object in use. */
-    protected Core core;
-
-    /** The bot for which this listener works. */
-    protected Bot bot;
-
-    /** The id of the bot for which this listener works. */
-    protected String botID;
-
-    /** The parameters that can be set for this listener. */
-    protected Map<String, String> parameters;
-
-    /** The logger to use. */
-    protected Logger logger;
-
-    /**
-     * Creates a new listener with name <code>name</code>.
-     * 
-     * @param coreToUse the Core object in use
-     * @param botToListenFor the bot for whom to listen
-     * @param listenerParameters the parameters for the listener and their
-     *            default values
-     */
-    public Listener(Core coreToUse, Bot botToListenFor, Map<String, String> listenerParameters)
-    {
-        this.core = coreToUse;
-        this.bot = botToListenFor;
-        this.botID = botToListenFor.getID();
-        this.parameters = listenerParameters;
-        this.logger = Logger.getLogger("programd");
-    }
-
     /**
      * Validates parameters.
      * 
      * @throws InvalidListenerParameterException if a parameter is not valid
      */
-    abstract public void checkParameters() throws InvalidListenerParameterException;
+    public void checkParameters() throws InvalidListenerParameterException;
 }
