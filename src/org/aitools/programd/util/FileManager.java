0efdbea8a6533a11301c714ffa6179a9342dc973
noel <>
2005-05-17 04:04:20 +0000

    small change to absolute path checking

--- src/org/aitools/programd/util/FileManager.java
+++ src/org/aitools/programd/util/FileManager.java
@@ -75,7 +75,7 @@ public class FileManager
                 throw new DeveloperError("Could not create canonical file for \"" + file.getAbsolutePath() + "\".", e);
             }
         }
-        if (path.indexOf('/') != 0)
+        if (path.indexOf(File.separator) != 0)
         {
             return new File(rootPath.getPath() + path);
         }
