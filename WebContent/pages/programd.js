230522e05c08d2cd5e313d541dda2b78ce726c04
noel <>
2005-09-23 15:22:30 +0000

    documented functions

--- WebContent/pages/programd.js
+++ WebContent/pages/programd.js
@@ -1,13 +1,24 @@
+/**
+ * Puts the cursor into the input field.
+ */
 function focusInput()
 {
 	var input = document.getElementById('user-input').focus();
 }
 
+/**
+ * Sends whatever is in the user-input field to the bot.
+ */
 function submit()
 {
 	sendToBot(DWRUtil.getValue('user-input'));
 }
 
+/**
+ * Sends a non-empty message to the bot, and sets up
+ * a callback (thanks to DWR) that will receive the
+ * response and display it.
+ */
 function sendToBot(message)
 {
 	if (message != '')
@@ -16,43 +27,75 @@ function sendToBot(message)
 	}
 }
 
+/**
+ * Displays a response sent from the bot, filling/changing
+ * the appropriate boxes on the page, and leaves the cursor
+ * in the input box.
+ */
 var displayResponse = function(response)
 {
+	// Put the response into the last-bot-reply div.
 	DWRUtil.setValue('last-bot-reply', response);
+	
+	// Grab the history box.
 	var history = document.getElementById('dialogue-history');
 
+	// Get the input.
 	var input = DWRUtil.getValue('user-input');
+	
+	// Don't bother with empty inputs.
 	if (input != '')
 	{
+		// Put the input into the last-user-input div.
 		DWRUtil.setValue('last-user-input', input);
 		
+		// Create a paragraph that will display the user input in the dialogue history.
 		var userinput = document.createElement('p');
 		userinput.setAttribute('class', 'user-input');
-		userinput.innerHTML = input;
+
+		// Make a label and append it.
 		var userinputLabel = document.createElement('span');
 		userinputLabel.setAttribute('class', 'label');
 		userinputLabel.appendChild(document.createTextNode('you> '));
-		userinput.insertBefore(userinputLabel, userinput.childNodes.item(0));
-
+		userinput.appendChild(userinputLabel);
+		
+		// Append the user input text.
+		userinput.appendChild(document.createTextNode(input));
+		
+		// Append this paragraph to the history.
 		history.appendChild(userinput);
 		
-		DWRUtil.setValue('user-input', null);
-		document.getElementById('last-user-input-box').style.display = 'block';
-	}
-	else
-	{
-		document.getElementById('last-user-input-box').style.display = 'none';
+		// Blank out the user input.
+		/*
+		 * TODO:
+		 * Need some way to notify the field that it has changed
+		 * (so onchange will work right if the same value is
+		 * typed again).
+		 */
+		DWRUtil.setValue('user-input', '');
 	}
 	
+	// Create a paragraph that will display the reply in the dialogue history.
 	var botreply = document.createElement('p');
 	botreply.setAttribute('class', 'bot-reply');
+
+	// We set it with (non-standard) .innerHTML so that HTML in the reply will be displayed.  Other ideas?
 	botreply.innerHTML = response;
+	
+	// Make a label.
 	var botreplyLabel = document.createElement('span');
 	botreplyLabel.setAttribute('class', 'label');
 	botreplyLabel.appendChild(document.createTextNode(botName + '> '));
+	
+	// Insert the label before the already-inserted text/html.
 	botreply.insertBefore(botreplyLabel, botreply.childNodes.item(0));
 	
+	// Append this paragraph.
 	history.appendChild(botreply);
+	
+	// Scroll to the bottom (so the history will be visible).
+	window.scrollTo(0, 1000000);
 
+	// Put the cursor back in the input field.
 	focusInput();
 }
