a8d2c06ae5ae29897dd2c618bf44348ce1d036ce
noel <>
2006-02-22 02:15:58 +0000

    got working on Windows again

--- bin/common_functions.bat
+++ bin/common_functions.bat
@@ -1,44 +1,44 @@
-rem ==========================================================================
-rem This program is free software; you can redistribute it and/or
-rem modify it under the terms of the GNU General Public License
-rem as published by the Free Software Foundation; either version 2
-rem of the License, or (at your option) any later version.
-rem
-rem You should have received a copy of the GNU General Public License
-rem along with this program; if not, write to the Free Software
-rem Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, 
-rem USA.
-rem ==========================================================================
+@rem ==========================================================================
+@rem This program is free software; you can redistribute it and/or
+@rem modify it under the terms of the GNU General Public License
+@rem as published by the Free Software Foundation; either version 2
+@rem of the License, or (at your option) any later version.
+@rem
+@rem You should have received a copy of the GNU General Public License
+@rem along with this program; if not, write to the Free Software
+@rem Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, 
+@rem USA.
+@rem ==========================================================================
 
-rem This contains functions used by batch scripts.
-rem This file itself is not meant to be executed.
-rem You must pass a parameter that corresponds to a labeled section of the batch file.
+@rem This contains functions used by batch scripts.
+@rem This file itself is not meant to be executed.
+@rem You must pass a parameter that corresponds to a labeled section of the batch file.
 
 goto %1
 
-rem Checks for needed environment space and increases it if necessary.
+@rem Checks for needed environment space and increases it if necessary.
 :check_env
   set ENVTEST=This just checks whether there is additional environment space.
   if "%ENVTEST%"=="" goto increase_env
   set ENVTEST=
 goto end
 
-rem Increases environment space.
+@rem Increases environment space.
 :increase_env
   echo Increasing environment space
   if not exist %comspec% goto nocomspec
 
 :comspec
-  rem %comspec% points to an existing command interpreter
+  @rem %comspec% points to an existing command interpreter
   %comspec% /E:4096 /C %2 %3 %4 %5
   goto end
 
 :nocomspec
-  rem %comspec% is not set, trying command.com
+  @rem %comspec% is not set, trying command.com
   command /E:4096 /C %2 %3 %4 %5
 goto end
 
-rem Sets the BASE variable to the directory above the bin directory.
+@rem Sets the BASE variable to the directory above the bin directory.
 :set_base
   pushd %~p0\..
   if %OS%'==Windows_NT' (for %%d in (.) do set BASE=%%~fd&goto done)
@@ -50,34 +50,34 @@ rem Sets the BASE variable to the directory above the bin directory.
   popd
 goto end
 
-rem Starts Program D using a given main class.
+@rem Starts Program D using a given main class.
 :start_programd
 
-  rem Set up the Program D variables.
+  @rem Set up the Program D variables.
   if "%quit%"=="" call %0 setup_programd
 
-  rem Set up the Java environment.
+  @rem Set up the Java environment.
   if "%quit%"=="" call %0 setup_java
 
-  rem Concatenate all paths into the classpath to be used.
+  @rem Concatenate all paths into the classpath to be used.
   set PROGRAMD_CLASSPATH=%PROGRAMD_LIBS%;%JS_LIB%;%SQL_LIB%
 
-  rem Change to the Program D directory and start the main class.
+  @rem Change to the Program D directory and start the main class.
   pushd %BASE%
   %JVM_COMMAND% -classpath %PROGRAMD_CLASSPATH% -Xms%3 -Xmx%4 %2 -c %5
 
-  rem On exit, leave the base directory.
+  @rem On exit, leave the base directory.
   :finished
   popd
 goto end
 
-rem Sets up some variables used to run/build Program D.
+@rem Sets up some variables used to run/build Program D.
 :setup_programd
 
-  rem Set lib directories
+  @rem Set lib directories
   call %0 setup_lib_dirs
   
-  set PROGRAMD_MAIN_LIB=%LIBS%\programd-main.jar
+  set PROGRAMD_MAIN_LIB=%DISTRIB%\programd-main.jar
   if exist %PROGRAMD_MAIN_LIB% goto check_other_programd_jars
   
   echo.
@@ -89,10 +89,10 @@ rem Sets up some variables used to run/build Program D.
   goto end
   
   :check_other_programd_jars
-  rem Define the other programd jars, but don't worry if they don't exist.
-  set PROGRAMD_JS_LIB=%LIBS%\programd-rhino.jar
+  @rem Define the other programd jars, but don't worry if they don't exist.
+  set PROGRAMD_JS_LIB=%DISTRIB%\programd-rhino.jar
   
-  rem Set up external jars.
+  @rem Set up external jars.
   call %0 setup_other_libs
   
   set PROGRAMD_LIBS=%PROGRAMD_MAIN_LIB%;%PROGRAMD_JS_LIB%;%OTHER_LIBS%
@@ -101,12 +101,13 @@ goto end
 :setup_other_libs
   
   set GETOPT_LIB=%LIBS%\gnu.getopt-1.0.10.jar
-  if exist %GETOPT_LIB% goto check_optional_components
+  if exist %GETOPT_LIB% goto set_log4j_lib
   echo.
   echo I can't find the gnu.getopt-1.0.10.jar that ships with Program D.
   set quit=yes
   goto end
   
+  :set_log4j_lib
   set LOG4J_LIB=%WEBLIBS%\log4j-1.2.13.jar
   if exist %LOG4J_LIB% goto check_optional_components
   echo.
@@ -115,55 +116,56 @@ goto end
   goto end
 
   :check_optional_components
-  rem Optional components:
+  @rem Optional components:
   
-  rem Set LISTENER_LIBS to the location of your listener jars.
-  rem No warning is provided if they cannot be found (since they are optional).
+  @rem Set LISTENER_LIBS to the location of your listener jars.
+  @rem No warning is provided if they cannot be found (since they are optional).
   set ICQ_AIM_LISTENER_LIBS=%LIBS%\icq-aim-listener\icq-aim-listener.jar;%LIBS%\icq-aim-listener\daim.jar;%LIBS%\icq-aim-listener\log4j-1.2.9.jar
   set IRC_LISTENER_LIBS=%LIBS%\irc-listener\irc-listener.jar
   set YAHOO_LISTENER_LIBS=%LIBS%\yahoo-listener\yahoo-listener.jar;%LIBS%\yahoo-listener\ymsg_network_v0_6.jar
   set LISTENER_LIBS=%ICQ_AIM_LISTENER_LIBS%;%IRC_LISTENER_LIBS%;%YAHOO_LISTENER_LIBS%
   set LISTENER_LIBS=%LIBS%\icq-aim-listener.jar
 
-  rem Set SQL_LIB to the location of your database driver.
-  rem No warning is provided if it cannot be found (since it is optional).
+  @rem Set SQL_LIB to the location of your database driver.
+  @rem No warning is provided if it cannot be found (since it is optional).
   set SQL_LIB=%LIBS%\mysql-connector-java-3.1.12-bin.jar
 
-  rem Set JS_LIB to the location of the Rhino JavaScript interpreter.
-  rem No warning is provided if it cannot be found (since it is optional).
-  set JS_LIB=%LIBS%\js.jar
+  @rem Set JS_LIB to the location of the Rhino JavaScript interpreter.
+  @rem No warning is provided if it cannot be found (since it is optional).
+  set JS_LIB=%WEBLIBS%\js.jar
   
   set OTHER_LIBS=%GETOPT_LIB%;%LOG4J_LIB%;%LISTENER_LIBS%;%SQL_LIB%;%JS_LIB%
 goto end
 
 
-rem Sets up the lib directories.
+@rem Sets up the lib directories.
 :setup_lib_dirs
   set LIBS=%BASE%\lib
+  set DISTRIB=%BASE%\distrib
   set WEBLIBS=%BASE%\WebContent\WEB-INF\lib
 goto end
 
-rem Sets up a Java execution environment
-rem (or fails informatively).
+@rem Sets up a Java execution environment
+@rem (or fails informatively).
 :setup_java
   set quit=
   if "%quit%"=="" call %0 set_java_vars
   if "%quit%"=="" call %0 check_java_home
   if "%quit%"=="" call %0 set_jvm_command
-  rem We don't check JVM version because
-  rem I don't know equivalent text manipulation
-  rem tools in DOS for parsing java -version output.
+  @rem We don't check JVM version because
+  @rem I don't know equivalent text manipulation
+  @rem tools in DOS for parsing java -version output.
 goto end
 
 
-rem Tries to find/set JAVA_HOME.
+@rem Tries to find/set JAVA_HOME.
 :set_java_vars
-  rem Try to find JAVA_HOME if it isn't already set.
+  @rem Try to find JAVA_HOME if it isn't already set.
   if defined JAVA_HOME goto end
 
   echo JAVA_HOME is not set in your environment.
 
-  rem Try the standard JDK 5.0 install location.
+  @rem Try the standard JDK 5.0 install location.
   if not exist c:\jdk1.5.0_02\bin\java.exe goto seek_known_javas
 
   set JAVA_HOME=c:Progra~1\Java\\jdk1.5.0_02\
@@ -171,7 +173,7 @@ rem Tries to find/set JAVA_HOME.
   goto successful
 
   :seek_known_javas
-  rem Common paths for compatible Java SDKs (or JREs) should go here.
+  @rem Common paths for compatible Java SDKs (or JREs) should go here.
   if exist d:Progra~1\Java\\jdk1.5.0_02\bin\java.exe set JAVA_HOME=d:\jdk1.5.0_02
    if not defined JAVA_HOME goto cannot_find
 
@@ -187,7 +189,7 @@ rem Tries to find/set JAVA_HOME.
 goto end
 
 
-rem Checks that JAVA_HOME points to a real directory.
+@rem Checks that JAVA_HOME points to a real directory.
 :check_java_home
   if exist "%JAVA_HOME%" goto end
   
@@ -201,7 +203,7 @@ rem Checks that JAVA_HOME points to a real directory.
 goto end
 
 :set_jvm_command
-  set JVM_COMMAND="%JAVA_HOME%\bin\java.exe -Dlog4j.configuration=file:%BASE%\JavaSource\log4j.xml"
+  set JVM_COMMAND="%JAVA_HOME%\bin\java.exe" -Dlog4j.configuration=file:/%BASE%\JavaSource\log4j.xml
 goto end
 
 :end
