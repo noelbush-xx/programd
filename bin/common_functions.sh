1c90f509475bd8cdc2d3af27e555a41e7f84853b
noel <>
2006-05-11 17:23:03 +0000

    majorly simplified

--- bin/common_functions.sh
+++ bin/common_functions.sh
@@ -14,89 +14,26 @@
 # This file itself is not executable.
 
 # Starts Program D using a given main class.
+# Parameters:
+# $1: jar file (with a main class specified internally by manifest)
+# $2: starting memory allocation
+# $3: maximum memory allocation
+# $4: configuration file
 function start_programd()
 {
-  # Set up the Program D variables.
+  # Set up Program D variables
   setup_programd
-
+  
   # Set up the Java environment.
   setup_java
-  
-  # Concatenate all paths into the classpath to be used.
-  PROGRAMD_CLASSPATH=$PROGRAMD_LIBS:$JS_LIB:$SQL_LIB
 
-  # Change to the Program D directory and start the main class.
+  # Change to the Program D directory and launch the given jar file..
   cd $BASE
-  $JVM_COMMAND -classpath $PROGRAMD_CLASSPATH -Xms$2 -Xmx$3 $1 -c $4
+  $JVM_COMMAND -Xms$2 -Xmx$3 -jar $1 -c $4
 }
 
 # Sets up some variables used to run Program D.
 function setup_programd()
-{
-  # Set lib directories
-  setup_lib_dirs
-  
-  PROGRAMD_MAIN_LIB=$DISTRIB/programd-main.jar
-  if [ ! -r $PROGRAMD_MAIN_LIB ]
-  then
-    echo I can\'t find your programd-main.jar file.  Have you compiled it?
-    echo If you downloaded the source-only version but don\'t have
-    echo a Java compiler, you can download a pre-compiled version from
-    echo http://aitools.org/downloads/
-    echo
-    exit 1
-  fi
-  
-  # Define the other programd jars, but don't worry if they don't exist.
-  PROGRAMD_JS_LIB=$DISTRIB/programd-rhino.jar
-  
-  # Set up external jars.
-  setup_other_libs
-
-  PROGRAMD_LIBS=$PROGRAMD_MAIN_LIB:$PROGRAMD_JS_LIB:$OTHER_LIBS
-}
-
-# Sets up other required included libs.
-function setup_other_libs()
-{
-  GETOPT_LIB=$LIBS/gnu.getopt-1.0.10.jar
-  if [ ! -r $GETOPT_LIB ]
-  then
-    echo
-    echo I can\'t find the gnu.getopt-1.0.10.jar that ships with Program D.
-    exit 1
-  fi
-
-  LOG4J_LIB=$WEBLIBS/log4j-1.2.13.jar
-  if [ ! -r $LOG4J_LIB ]
-  then
-    echo
-    echo I can\'t find the log4j-1.2.13.jar that ships with Program D.
-    exit 1
-  fi
-
-  # Optional components:
-
-  # Set LISTENER_LIBS to the location of your listener jars.
-  # No warning is provided if they cannot be found (since they are optional).
-  AIM_LISTENER_LIBS=$LIBS/aim-listener/aim-listener.jar:$LIBS/aim-listener/jaimbot-lib-1.4.jar
-  IRC_LISTENER_LIBS=$LIBS/irc-listener/irc-listener.jar:$LIBS/irc-listener/pircbot.jar
-  YAHOO_LISTENER_LIBS=$LIBS/yahoo-listener/yahoo-listener.jar:$LIBS/yahoo-listener/ymsg_network_v0_61.jar
-  LISTENER_LIBS=$AIM_LISTENER_LIBS:$IRC_LISTENER_LIBS:$YAHOO_LISTENER_LIBS
-
-  # Set SQL_LIB to the location of your database driver.
-  # No warning is provided if it cannot be found (since it is optional).
-  SQL_LIB=$LIBS/mysql-connector-java-3.1.12-bin.jar
-
-  # Set JS_LIB to the location of the Rhino JavaScript interpreter.
-  # No warning is provided if it cannot be found (since it is optional).
-  JS_LIB=$WEBLIBS/js.jar
-  
-  OTHER_LIBS=$GETOPT_LIB:$LOG4J_LIB:$LISTENER_LIBS:$SQL_LIB:$JS_LIB
-}
-
-# Sets up the lib directories.
-function setup_lib_dirs()
 {
   LIBS=$BASE/lib
   DISTRIB=$BASE/distrib
