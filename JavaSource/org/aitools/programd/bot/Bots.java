41e1ddab71b1dd7bd151e5a35aa5a7b82d62ef36
noel <>
2006-06-10 04:25:48 +0000

    make this extend (instead of wrap a) HashMap, and regularize method names

--- JavaSource/org/aitools/programd/bot/Bots.java
+++ JavaSource/org/aitools/programd/bot/Bots.java
@@ -9,77 +9,15 @@
 
 package org.aitools.programd.bot;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-
-import org.aitools.util.runtime.DeveloperError;
 
 /**
  * Contains all descriptions of bots.
  * 
  * @author <a href="mailto:noel@aitools.org">Noel Bush</a>
  */
-public class Bots
+public class Bots extends HashMap<String, Bot>
 {
-    /** The map of bot ids to bots. */
-    private Map<String, Bot> botMap;
-
-    /**
-     * Creates a new <code>Bots</code>.
-     */
-    public Bots()
-    {
-        this.botMap = Collections.checkedMap(new HashMap<String, Bot>(), String.class, Bot.class);
-    }
-
-    /**
-     * Returns whether the loaded bots include one with the given id.
-     * 
-     * @param botid the botid to look for
-     * @return whether the loaded bots include one with the given id
-     */
-    public boolean include(String botid)
-    {
-        return this.botMap.containsKey(botid);
-    }
-
-    /**
-     * Adds the given bot with the given id. No check is made to see whether a bot is already loaded with the given id!
-     * 
-     * @param botid the id to use for the bot
-     * @param bot the bot to add
-     */
-    public void addBot(String botid, Bot bot)
-    {
-        this.botMap.put(botid, bot);
-    }
-
-    /**
-     * Returns the bot with the given id.
-     * 
-     * @param botid the id of the bot to return
-     * @return the bot with the given id
-     */
-    public Bot getBot(String botid)
-    {
-        Bot wanted;
-        try
-        {
-            wanted = this.botMap.get(botid);
-        }
-        catch (ClassCastException e)
-        {
-            throw new DeveloperError("Something other than a Bot stored in Bots!", e);
-        }
-        if (wanted == null)
-        {
-            throw new DeveloperError("Tried to get unknown bot \"" + botid + "\".", new NullPointerException());
-        }
-        return wanted;
-    }
 
     /**
      * Returns any bot (probably the last one loaded).
@@ -88,21 +26,11 @@ public class Bots
      */
     public Bot getABot()
     {
-        if (this.botMap.size() > 0)
+        if (this.size() > 0)
         {
-            return this.botMap.values().iterator().next();
+            return this.values().iterator().next();
         }
         return null;
-    }
-
-    /**
-     * Returns the number of bots (the size)
-     * 
-     * @return the number of bots (the size)
-     */
-    public int getCount()
-    {
-        return this.botMap.size();
     }
 
     /**
@@ -110,14 +38,15 @@ public class Bots
      * 
      * @return a nicely-formatted list of the bots
      */
-    public String getNiceList()
+    @Override
+    public String toString()
     {
-        if (this.botMap.size() == 0)
+        if (this.size() == 0)
         {
             return "";
         }
         StringBuilder result = new StringBuilder();
-        for (String botName : this.botMap.keySet())
+        for (String botName : this.keySet())
         {
             if (result.length() > 0)
             {
@@ -129,26 +58,6 @@ public class Bots
     }
 
     /**
-     * Returns the IDs (the key set)
-     * 
-     * @return the IDs (the key set)
-     */
-    public Set<String> getIDs()
-    {
-        return this.botMap.keySet();
-    }
-
-    /**
-     * Returns an iterator over the key set
-     * 
-     * @return an iterator over the key set
-     */
-    public Iterator<String> keysIterator()
-    {
-        return this.botMap.keySet().iterator();
-    }
-
-    /**
      * Returns whether any bots have loaded the given file(name).
      * 
      * @param filename
@@ -156,7 +65,7 @@ public class Bots
      */
     public boolean haveLoaded(String filename)
     {
-        for (Bot bot : this.botMap.values())
+        for (Bot bot : this.values())
         {
             if (bot.hasLoaded(filename))
             {
@@ -164,15 +73,5 @@ public class Bots
             }
         }
         return false;
-    }
-
-    /**
-     * Removes the indicated bot.
-     * 
-     * @param id
-     */
-    public void removeBot(String id)
-    {
-        this.botMap.remove(id);
     }
 }
