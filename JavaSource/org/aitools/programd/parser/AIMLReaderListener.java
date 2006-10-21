622e7c726b402035e2afeb62193847546e1a5abb
noel <>
2006-10-21 12:31:44 +0000

    reflect abstraction of Graphmaster to Graphmapper

--- JavaSource/org/aitools/programd/parser/AIMLReaderListener.java
+++ JavaSource/org/aitools/programd/parser/AIMLReaderListener.java
@@ -18,7 +18,7 @@ package org.aitools.programd.parser;
 public interface AIMLReaderListener
 {
     /**
-     * Adds a new category to the {@link org.aitools.programd.graph.Graphmaster} . Usually called by an AIML parser such
+     * Adds a new category to the {@link org.aitools.programd.graph.Graphmapper} . Usually called by an AIML parser such
      * as {@link org.aitools.programd.parser.TemplateParser TemplateParser} .
      * 
      * @param pattern the <code>pattern</code> portion of a Graphmaster path
